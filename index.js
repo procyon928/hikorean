bd27f1c8fe98696a7a0f92b968f4a14130c7998d
procyon928 <procyon928@gmail.com>
2024-08-09 13:28:06 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
+    console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다리.`);
 });
