0f242bcda03535af66f8195f69c4beb2028f1a24
procyon928 <procyon928@gmail.com>
2024-08-09 15:59:21 +0900

    port 3000 변경

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('안녕하세요! Node.js 웹 서버에 오신 것을 환영합니다.');
