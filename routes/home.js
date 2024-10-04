40855e308ff19cfc6e49a0770f188b75253f928e
procyon928 <procyon928@gmail.com>
2024-10-04 12:11:53 +0900

    홈화면 휴일 추가

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,30 @@
 const express = require('express');
 const router = express.Router();
 const Schedule = require('../models/Schedule');
+const fs = require('fs');
+
+// 휴일 목록 불러오기
+const holidaysArray = JSON.parse(fs.readFileSync('data/holidays.json', 'utf-8'));
+
+// 객체로 변환 (optional)
+const holidays = holidaysArray.map(item => ({
+    date: item[0],
+    name: item[1]
+}));
 
 // 홈 페이지 렌더링
 router.get('/', async (req, res) => {
   try {
       const schedules = await Schedule.find(); // 모든 스케줄 가져오기
+
+      // 오늘 날짜를 KST로 변환
+      const today = new Date();
+      const timezoneOffset = today.getTimezoneOffset() * 60 * 1000; // UTC 오프셋을 밀리초로 변환
+      const koreaOffset = 9 * 60 * 60 * 1000; // KST 오프셋 (9시간)
+      const todayKST = new Date(today.getTime() + timezoneOffset + koreaOffset); // 오늘 날짜를 KST로 변환
+
       console.log('Schedules:', schedules); // 추가된 로그
-      res.render('home', { schedules }); // 스케줄 데이터를 뷰로 전달
+      res.render('home', { schedules, todayKST, holidays }); // 스케줄 데이터와 todayKST를 뷰로 전달
   } catch (err) {
       console.error(err);
       res.status(500).send(err.message);
@@ -30,6 +47,5 @@ router.get('/schedules', async (req, res) => {
       res.status(500).send(err.message);
   }
 });
-
 
 module.exports = router;
