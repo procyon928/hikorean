2c9630a3e7d890c8bef30b08df11b1e15d71d203
wonjunChoi <procyon928@gmail.com>
2024-10-07 02:29:09 +0900

    계정탈퇴기능 추가

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { User } = require('../models/User'); // User 모델 가져오기
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); // 비밀번호 해싱을 위한 bcrypt
 const router = express.Router();
+const axios = require('axios'); // axios 패키지 추가
 
 // 회원 페이지
 router.get('/account', (req, res) => {
@@ -16,7 +17,7 @@ router.get('/account', (req, res) => {
 router.post('/account/update', async (req, res) => {
     const { username, email } = req.body;
     try {
-        await User.updateOne({ _id: req.session.user.id }, { username, email });
+        await User.updateOne({ _id: req.user._id }, { username, email }); // req.user로 수정
         req.session.user.username = username; // 세션 정보 업데이트
         req.session.user.email = email;
         res.redirect('/account'); // 수정 후 회원 페이지로 리다이렉트
@@ -30,7 +31,7 @@ router.post('/account/update', async (req, res) => {
 router.post('/account/change-password', async (req, res) => {
     const { currentPassword, newPassword } = req.body;
     try {
-        const user = await User.findById(req.session.user.id);
+        const user = await User.findById(req.user._id); // req.user로 수정
         const isMatch = await bcrypt.compare(currentPassword, user.password);
         if (!isMatch) {
             return res.status(401).send('현재 비밀번호가 잘못되었습니다.');
@@ -52,6 +53,46 @@ router.post('/logout', (req, res) => {
         }
         res.redirect('/'); // 로그아웃 후 메인 페이지로 리다이렉트
     });
+});
+
+// 계정 삭제 처리
+router.post('/account/delete', async (req, res) => {
+  const user = req.user; // 현재 로그인한 사용자 정보
+
+  try {
+      if (user.authProvider === 'google') {
+          // 구글 사용자: 동의 철회 요청
+          const accessToken = user.refreshToken; // 사용자의 refreshToken 사용
+
+          // Google API에서 동의 철회
+          await axios.post(`https://oauth2.googleapis.com/revoke?token=${accessToken}`)
+              .then(response => {
+                  console.log('동의 철회 성공:', response.data);
+              })
+              .catch(err => {
+                  console.error('동의 철회 오류:', err);
+              });
+      }
+
+      // DB에서 사용자 삭제
+      await User.deleteOne({ _id: user._id });
+
+      // 세션 종료
+      req.logout((err) => { // 콜백 함수 추가
+          if (err) {
+              return res.status(500).send('로그아웃 중 오류가 발생했습니다.');
+          }
+          req.session.destroy((err) => {
+              if (err) {
+                  return res.status(500).send('로그아웃 중 오류가 발생했습니다.');
+              }
+              res.redirect('/'); // 메인 페이지로 리다이렉트
+          });
+      });
+  } catch (error) {
+      console.error('계정 삭제 오류:', error);
+      res.status(500).send('계정 삭제 중 오류가 발생했습니다.');
+  }
 });
 
 module.exports = router;
